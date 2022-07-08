6306fec0b215af6fc68b7460715420a89bee6c9e
Mo-Aziz <mohamed.aref32@gmail.com>
2022-07-08 11:44:22 -0500

    adjusted some styling props

--- src/components/cloud/Cloud.js
+++ src/components/cloud/Cloud.js
@@ -5,7 +5,7 @@ import './CloudStyles.css'
  const Cloud = () => {
   return (
     <div className='cloud'>
-        <div container>
+        <div className='container'>
             <div className='content'>
                 <h2><span>Cloud</span> Security</h2>
                 <p>Cloud computing security or, more simply, cloud security refers to a broad set of policies, technologies, applications, and controls utilized to protect virtualized IP, data, applications, services, and the associated infrastructure of cloud computing.</p>
